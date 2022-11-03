2e19c1c44bf051269971acaf09d56b2c18d3b470
byungmeo <wlsrlfvkr@gmail.com>
2022-11-04 00:17:39 +0900

    Innit Commit

---
+++ DungeonInTextOnline/DungeonInText_Client/DungeonInText_Client.cpp
@@ -0,0 +1,33 @@
+﻿#include <hiredis/hiredis.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Client" << endl;
+
+    redisContext* c = redisConnect("127.0.0.1", 6379);
+    if (c == NULL || c->err) {
+        if (c) {
+            printf("Error: %s\n", c->errstr);
+            //에러 처리
+        } else {
+            printf("Can't allocate redis context\n");
+        }
+    }
+
+    redisReply* reply = (redisReply*)redisCommand(c, "SET USER:byungmeo 1");
+    if (reply->type == REDIS_REPLY_ERROR) {
+        //에러처리
+    }
+
+    reply = (redisReply*)redisCommand(c, "GET USER:byungmeo");
+    if (reply->type == REDIS_REPLY_STRING) {
+        cout << "결과 : " << reply->str << endl;
+    } else if (reply->type == REDIS_REPLY_ERROR) {
+        //에러처리
+    }
+
+    freeReplyObject(reply);
+    redisFree(c);
+}
