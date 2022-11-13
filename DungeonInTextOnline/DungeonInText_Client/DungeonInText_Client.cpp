e96ceac7ba1628793080cd5160850997efb9360c
byungmeo <wlsrlfvkr@gmail.com>
2022-11-13 16:25:26 +0900

    Client : 명령어를 JSON으로 변환하여 전송

--- DungeonInTextOnline/DungeonInText_Client/DungeonInText_Client.cpp
+++ DungeonInTextOnline/DungeonInText_Client/DungeonInText_Client.cpp
@@ -16,6 +16,67 @@ using namespace std;
 
 static unsigned short SERVER_PORT = 27015;
 
+string moveToJson(int x, int y) {
+    char jsonData[UCHAR_MAX];
+    sprintf_s(jsonData, sizeof(jsonData), "{\"command\": \"move\", \"userName\": \"unknown\", \"x\": %d, \"y\": %d}", x, y);
+    return jsonData;
+}
+
+string chatToJson(string dest, string msg) {
+    char jsonData[UCHAR_MAX];
+    sprintf_s(jsonData, sizeof(jsonData), "{\"command\": \"chat\", \"userName\": \"unknown\", \"dest\": \"%s\", \"msg\": \"%s\"}", dest.c_str(), msg.c_str());
+    return jsonData;
+}
+
+string otherToJson(string command) {
+    char jsonData[UCHAR_MAX];
+    sprintf_s(jsonData, sizeof(jsonData), "{\"command\": \"%s\", \"userName\": \"unknown\"}", command.c_str());
+    return jsonData;
+}
+
+string inputCommandJson() {
+    string input, jsonData;
+    while (true) {
+        cin >> input;
+        if (input.compare("move") == 0) {
+            int x, y;
+            cin >> x;
+            if (cin.fail() || abs(x) > 3) {
+                cerr << "잘못된 좌표 입력" << endl;
+                cin.clear(); // 에러 비트 초기화
+                cin.ignore(UCHAR_MAX, '\n'); // 버퍼를 비운다
+                continue; // 처음부터 다시 입력
+            }
+            cin >> y;
+            if (cin.fail() || abs(y) > 3) {
+                cerr << "잘못된 좌표 입력" << endl;
+                cin.clear(); //에러 비트 초기화
+                cin.ignore(UCHAR_MAX, '\n'); //버퍼를 비운다
+                continue; // 처음부터 다시 입력
+            }
+            jsonData = moveToJson(x, y);
+            break;
+        } else if (input.compare("chat") == 0) {
+            string dest, msg;
+            cin >> dest;
+            getline(cin, msg);
+            jsonData = chatToJson(dest, msg.c_str() + 1);
+            break;
+        } else if (input.compare("attack") == 0 || input.compare("bot") == 0 || input.compare("monsters") == 0 || input.compare("users") == 0) {
+            jsonData = otherToJson(input);
+            break;
+        } else if (input.compare("exit") == 0) {
+            return "exit";
+        } else {
+            cerr << "잘못된 명령어" << endl;
+            cin.ignore(UCHAR_MAX, '\n'); //버퍼를 비운다
+            continue;
+        }
+    }
+
+    return jsonData;
+}
+
 int main() {
     cout << "Client" << endl;
 
@@ -50,16 +111,21 @@ int main() {
     }
 
     // TODO: ID 입력 및 전송 구현
+    /*
+    */
 
-    // 1초 간격으로 계속 패킷을 보내본다.
     while (true) {
-        // 명령어 입력
+        // JSON으로 변환된 명령어를 입력받음
         string command;
-        getline(cin, command);
-
-        // TODO: 명령어 문자열을 JSON으로 변환
+        command = inputCommandJson();
+        if (command.compare("exit") == 0) {
+            cout << "Client를 종료 합니다." << endl;
+            break;
+        }
         
-        int dataLen = command.length() + (size_t)1; // 문자열의 끝을 의미하는 NULL 문자 포함
+        // TODO: 서버로부터 로그 수신 구현
+
+        int dataLen = command.length() + 1; // 문자열의 끝을 의미하는 NULL 문자 포함
 
         // 길이를 먼저 보낸다.
         // binary 로 4bytes 를 길이로 encoding 한다.
